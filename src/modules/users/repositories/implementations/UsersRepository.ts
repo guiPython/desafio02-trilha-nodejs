b921131188873a04fb92fdffe7aaec036531af8e
Guilherme Rocha <GuiPython2001@gmail.com>
2021-12-09 19:27:08 -0300

    feat: Add User Repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,30 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    Object.assign(user, {name, email});
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find(u => u.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find(u => u.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.users.find(u => u.id === receivedUser.id);
+    if(user){
+      user.admin = true;
+      return user;
+    }
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
